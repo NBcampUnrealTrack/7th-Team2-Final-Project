f731ebaa0b34ee6ae3c489d81daead96d427b36a
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-05-15 16:41:26 +0900

    [장유진/스켈레톤] RetrieveAbilitySet 작성

---
+++ Source/Retrieve/AbilitySystem/RetrieveAbilitySet.cpp
@@ -0,0 +1,122 @@
+#include "AbilitySystem/RetrieveAbilitySet.h"
+#include "AbilitySystem/RetrieveGameplayAbility.h"
+#include "RetrieveAbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "AttributeSet.h"
+
+void FRetrieveAbilitySet_GrantedHandles::AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle)
+{
+	if (Handle.IsValid())
+	{
+		AbilitySpecHandles.Add(Handle);
+	}
+}
+
+void FRetrieveAbilitySet_GrantedHandles::AddGameplayEffectHandle(const FActiveGameplayEffectHandle& Handle)
+{
+	if (Handle.IsValid())
+	{
+		GameplayEffectHandles.Add(Handle);
+	}
+}
+
+void FRetrieveAbilitySet_GrantedHandles::AddAttributeSet(UAttributeSet* Set)
+{
+	GrantedAttributeSets.Add(Set);
+}
+
+void FRetrieveAbilitySet_GrantedHandles::TakeFromAbilitySystem(URetrieveAbilitySystemComponent* ASC)
+{
+	if (!IsValid(ASC) || !ASC->IsOwnerActorAuthoritative())
+	{
+		return;
+	}
+
+	for (const FGameplayAbilitySpecHandle& Handle : AbilitySpecHandles)
+	{
+		if (Handle.IsValid())
+		{
+			ASC->ClearAbility(Handle);
+		}
+	}
+	for (const FActiveGameplayEffectHandle& Handle : GameplayEffectHandles)
+	{
+		if (Handle.IsValid())
+		{
+			ASC->RemoveActiveGameplayEffect(Handle);
+		}
+	}
+	for (UAttributeSet* Set : GrantedAttributeSets)
+	{
+		ASC->RemoveSpawnedAttribute(Set);
+	}
+
+	AbilitySpecHandles.Reset();
+	GameplayEffectHandles.Reset();
+	GrantedAttributeSets.Reset();
+}
+
+void URetrieveAbilitySet::GiveToAbilitySystem(URetrieveAbilitySystemComponent* ASC,
+                                              FRetrieveAbilitySet_GrantedHandles* OutGrantedHandles,
+                                              UObject* SourceObject) const
+{
+	check(ASC);
+	if (!ASC->IsOwnerActorAuthoritative()) return;
+
+	// Grant the attribute sets
+	for (const FRetrieveAbilitySet_AttributeSet& SetToGrant : GrantedAttributes)
+	{
+		if (!IsValid(SetToGrant.AttributeSet))
+		{
+			continue;
+		}
+
+		UAttributeSet* NewSet = NewObject<UAttributeSet>(ASC->GetOwner(), SetToGrant.AttributeSet);
+		ASC->AddSpawnedAttribute(NewSet);
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddAttributeSet(NewSet);
+		}
+	}
+
+	// Grant the gameplay abilities
+	for (const FRetrieveAbilitySet_GameplayAbility& AbilityToGrant : GrantedGameplayAbilities)
+	{
+		if (!IsValid(AbilityToGrant.Ability))
+		{
+			continue;
+		}
+
+		URetrieveGameplayAbility* AbilityCDO = AbilityToGrant.Ability->GetDefaultObject<URetrieveGameplayAbility>();
+
+		FGameplayAbilitySpec Spec(AbilityCDO, AbilityToGrant.AbilityLevel);
+		Spec.SourceObject = SourceObject;
+		Spec.GetDynamicSpecSourceTags().AddTag(AbilityToGrant.InputTag);
+
+		const FGameplayAbilitySpecHandle Handle = ASC->GiveAbility(Spec);
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddAbilitySpecHandle(Handle);
+		}
+	}
+
+	// Grant the gameplay effects
+	for (const FRetrieveAbilitySet_GameplayEffect& EffectToGrant : GrantedGameplayEffects)
+	{
+		if (!IsValid(EffectToGrant.GameplayEffect))
+		{
+			continue;
+		}
+
+		const UGameplayEffect* GameplayEffect = EffectToGrant.GameplayEffect->GetDefaultObject<UGameplayEffect>();
+		const FActiveGameplayEffectHandle Handle = ASC->ApplyGameplayEffectToSelf(
+			GameplayEffect, EffectToGrant.EffectLevel, ASC->MakeEffectContext());
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddGameplayEffectHandle(Handle);
+		}
+	}
+}
